e58a804119a4c6d4aa59312ee801dcb14b51495d
Ezequiel Pássaro <--global>
2021-01-18 03:07:08 -0300

    minor changes to resizer script

--- utils/resizer.py
+++ utils/resizer.py
@@ -1,25 +1,23 @@
 #! /usr/bin/python3
 
-import argparse
-import glob
 import os
+import glob
+import argparse
 from PIL import Image
 
 MOD_PATH = os.path.dirname(__file__)
 
-if __name__ == '__main__':
-    
+if __name__ == "__main__":
+
     parser = argparse.ArgumentParser(description="Resize photos keeping EXIF metadata")
-    parser.add_argument('max_size', help='max size (width or height)', type=int)
+    parser.add_argument("max_size", help="max size (width or height)", type=int)
     args = parser.parse_args()
-    
-    _ = [ os.rename(f, f.replace('JPG', 'jpg')) for f in glob.iglob(os.path.join(MOD_PATH, '*.JPG'))]
-    fnames = [ f for f in glob.iglob(os.path.join(MOD_PATH, '*.jpg'))]
-    
+
+    fnames = [f for f in glob.iglob(os.path.join(MOD_PATH, "*.[jJ][pP][gG]"))]
     for fname in fnames:
-        
+
         im = Image.open(fname)
-        exif = im.info['exif']
-        
+        exif = im.info["exif"]
+
         im.thumbnail((args.max_size, args.max_size))
         im.save(fname, exif=exif)
